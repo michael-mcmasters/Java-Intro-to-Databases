8bfcf570213ca6a2b2ced253f2728c076a04eadc
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-05 19:15:54 -0500

    Create empty methods for AddressBook

---
+++ src/main/java/com/codedifferently/AddressBook.java
@@ -0,0 +1,41 @@
+package com.codedifferently;
+
+import java.util.List;
+
+public class AddressBook {
+
+    private Person owner;
+    private List<Person> people;
+
+    public AddressBook() {
+
+    }
+
+    public Person getOwner() {
+        return null;
+    }
+
+    public void setOwner(Person person) {
+
+    }
+
+    public void addPerson(Person person) {
+
+    }
+
+    public void removePerson(Person person) {
+
+    }
+
+    public Person getPersonByEmail(String str) {
+        return null;
+    }
+
+    public List<Person> getAllPeople() {
+        return null;
+    }
+
+    public boolean saveAll() {
+        return false;
+    }
+}
