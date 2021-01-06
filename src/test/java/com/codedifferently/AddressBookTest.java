c3e870c7a4ae9facd6e016fff492003da453433b
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-05 19:22:38 -0500

    Create AddressBookTest but did not write tests

---
+++ src/test/java/com/codedifferently/AddressBookTest.java
@@ -0,0 +1,22 @@
+package com.codedifferently;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddressBookTest {
+
+    AddressBook addressBook;
+
+    // Need to create tests for Main first.
+
+//    @Before
+//    public void init() {
+//        addressBook = new AddressBook();
+//    }
+//
+//    @Test
+//    public void getOwnerTest() {
+//
+//    }
+
+}
